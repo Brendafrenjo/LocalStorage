3d75e323e0939b4f7ed3c8676d300dd5073c84e9
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-12-29 22:48:22 +0300

    Updated app.js

---
+++ app.js
@@ -0,0 +1,53 @@
+//DOM Elements
+
+const studentForm = document.getElementById("studentForm");
+const studentContainer = document.querySelector(".students");
+const nameInput = studentForm["name"];
+const ageInput = studentForm["age"];
+const rollInput = studentForm["roll"];
+
+const students = [];
+
+const addStudent = (name, age, roll) => {
+  const newStudent = {
+    name,
+    age,
+    roll,
+  };
+  students.push(newStudent);
+  return newStudent;
+};
+
+const createStudentElement = ({ name, age, roll }) => {
+  //create Elements
+  const studentDiv = document.createElement("div");
+  const studentName = document.createElement("h2");
+  const studentAge = document.createElement("p");
+  const studentRoll = document.createElement("p");
+
+  //Fill the content of the created elements
+  studentName.innerText = "Student name: " + name;
+  studentAge.innerText = "Student age: " + age;
+  studentRoll.innerText = "Student roll number: " + roll;
+
+  //Add created elements to the DOM
+  studentDiv.append(studentName, studentAge, studentRoll);
+  studentContainer.appendChild(studentDiv);
+};
+
+students.forEach(createStudentElement);
+
+studentForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const newStudent = addStudent(
+    nameInput.value,
+    ageInput.value,
+    rollInput.value
+  );
+  createStudentElement(newStudent);
+
+  nameInput.value = "";
+  ageInput.value = "";
+  rollInput.value = "";
+});
